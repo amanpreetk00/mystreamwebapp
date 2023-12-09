a4088d35b7f5568bb57056d7086c73bc194fda19
AmanPreet <Amanpreet_Kaur-AK2@ulster.ac.uk>
2023-12-09 14:30:48 +0000

    authorization added

--- JS/auth.js
+++ JS/auth.js
@@ -20,6 +20,12 @@ function signIn() {
   });
 }
 
+function joinNow() {
+  msalInstance.loginRedirect({
+    scopes: ["openid", "profile"],
+  });
+}
+
 function signOut() {
   msalInstance.logout();
 }
@@ -55,19 +61,40 @@ function hideContentForUnauthenticatedUser() {
 }
 
 // Update the handleResponse function
+// function handleResponse(response) {
+//   if (response !== null && response.account !== null) {
+//     sessionStorage.setItem("msalAccount", response.account.username);
+//     showWelcomeMessage(response.account);
+//   } else {
+//     const currentAccounts = msalInstance.getAllAccounts();
+//     if (!currentAccounts || currentAccounts.length < 1) {
+//       hideContentForUnauthenticatedUser();
+//       return;
+//     }
+//     showWelcomeMessage(currentAccounts[0]);
+//   }
+// }
+
 function handleResponse(response) {
   if (response !== null && response.account !== null) {
     sessionStorage.setItem("msalAccount", response.account.username);
     showWelcomeMessage(response.account);
-  } else {
-    const currentAccounts = msalInstance.getAllAccounts();
-    if (!currentAccounts || currentAccounts.length < 1) {
-      hideContentForUnauthenticatedUser();
-      return;
+
+    // Check if the logged-in user's email matches the specific email
+    if (response.account.username === "sacharya979@gmail.com") {
+      // Display upload form
+      document.getElementById("uploadForm").style.display = "block";
+      document.getElementById("watchVideos").style.display = "none";
+    } else if (response.account.username === "secondemail@example.com") {
+      // Hide upload form
+      document.getElementById("uploadForm").style.display = "none";
+      document.getElementById("watchVideos").style.display = "block";
     }
-    showWelcomeMessage(currentAccounts[0]);
+  } else {
+    hideContentForUnauthenticatedUser();
   }
 }
+
 msalInstance
   .handleRedirectPromise()
   .then(handleResponse)
